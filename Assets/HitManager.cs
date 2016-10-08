253122bec65bdeefc4ebc9c93ae51028ead0b059
Connor Smith <c3smith@ucsd.edu>
2016-10-08 18:56:03 -0400

    Hopefully fixed broken audio

--- Assets/HitManager.cs
+++ Assets/HitManager.cs
@@ -93,6 +93,8 @@ public class HitManager : MonoBehaviour {
     }
     public IEnumerator tutorialSequence() {
 
+        audioSource.clip = beat;
+
         tutorialText.text = TUTORIAL_START;
         yield return StartCoroutine(bottom.WaitForBatonTouch());
         audioSource.Play();
@@ -103,11 +105,11 @@ public class HitManager : MonoBehaviour {
 
         bottom.timesHit = 0;
 
+
         while(bottom.timesHit < 10) {
 
-            audioSource.clip = beat;
-
             bottom.ColorHit();
+
             audioSource.Play();
 
             yield return new WaitForSeconds(1.5f);
@@ -153,34 +155,50 @@ public class HitManager : MonoBehaviour {
             float timeBetweenBeats = 1.0f / beatsPerSecond;
 
             bottom.ColorHit();
+            audioSource.Play();
+
 
             yield return new WaitForSeconds(timeBetweenBeats / 2.0f);
 
             center.ColorHit();
+            audioSource.Play();
+
 
             yield return new WaitForSeconds(timeBetweenBeats / 2.0f);
 
             bottom.ColorHit();
+            audioSource.Play();
+
 
             yield return new WaitForSeconds(timeBetweenBeats / 2.0f);
 
 
             left.ColorHit();
+            audioSource.Play();
+
 
             yield return new WaitForSeconds(timeBetweenBeats / 2.0f);
             bottom.ColorHit();
+            audioSource.Play();
+
 
             yield return new WaitForSeconds(timeBetweenBeats / 2.0f);
 
             right.ColorHit();
+            audioSource.Play();
+
 
             yield return new WaitForSeconds(timeBetweenBeats / 2.0f);
 
             bottom.ColorHit();
+            audioSource.Play();
+
 
             yield return new WaitForSeconds(timeBetweenBeats / 2.0f);
 
             top.ColorHit();
+            audioSource.Play();
+
 
             yield return new WaitForSeconds(timeBetweenBeats / 2.0f);
 
