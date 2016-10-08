6937be8ec08063b71a305db75303e9f33cfd322b
Connor Smith <c3smith@ucsd.edu>
2016-10-08 12:23:06 -0400

    Let's make music!

---
+++ Assets/SongDriver.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SongDriver : MonoBehaviour {
+
+    public string songName = "[Default Song Name]";
+    public AudioClip songAudio;
+    public int bpm = 120;
+    public int beatsInMeasure = 4;
+    public int noteGetsBeat = 4;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
