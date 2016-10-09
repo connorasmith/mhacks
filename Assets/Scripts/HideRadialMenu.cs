eae4970ddbc629976fd747f195338286878f6699
Connor Smith <c3smith@ucsd.edu>
2016-10-09 05:09:06 -0400

    Hide radial menu

---
+++ Assets/Scripts/HideRadialMenu.cs
@@ -0,0 +1,32 @@
+﻿namespace VRTK.Examples {
+
+    using UnityEngine;
+    using System.Collections;
+
+    public class HideRadialMenu : MonoBehaviour {
+        public GameObject radialMenu;
+
+        // Use this for initialization
+        void Start() {
+
+            GetComponent<VRTK_ControllerEvents>().TriggerPressed += new ControllerInteractionEventHandler(showMenu);
+            GetComponent<VRTK_ControllerEvents>().TriggerReleased += new ControllerInteractionEventHandler(hideMenu);
+
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        private void showMenu(object sender, ControllerInteractionEventArgs e) {
+            radialMenu.SetActive(true);
+        }
+
+        private void hideMenu(object sender, ControllerInteractionEventArgs e) {
+            radialMenu.SetActive(false);
+
+        }
+    }
+}
